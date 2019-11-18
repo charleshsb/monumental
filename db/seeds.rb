3149a7639fad8d18a752d4c18eeabfa9235a4678
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-18 16:56:54 +0100

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Monument.create(title: 'The Louvre', address: 'Rue de Rivoli, 75001 Paris', description: "The world's largest art museum and a historic monument in Paris.", price: "500 000", photo: "https://images.unsplash.com/photo-1499634231146-3393ed854a33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80")
+
+Monument.create(title: 'The Eiffel Tower', address: 'Rue de Rivoli, 75001 Paris', description: "A wrought-iron lattice tower on the Champ de Mars in Paris. It is the most-visited paid monument in the world.", price: "700 000", photo: "https://images.unsplash.com/photo-1473205957495-db0d2dcb2a77?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80")
+
+Monument.create(title: 'Virgin Mary statue', address: 'San Cristobald Hill, Santiago de Chile', description: "One of the principal places of worship for the Catholic Church of Chile, as well as being an icon of Santiago, Chile. It is notable in particular for a large statue of the Immaculate Conception, which is located at the summit of San Cristóbal Hill, some 863 meters above sea level.", price: "200 000")
+
+Monument.create(title: 'Central Park', address: 'Manhattan, New York City, USA', description: "Central Park is an urban park in Manhattan, located between the Upper West Side and the Upper East Side. Central Park is the most visited urban park in the United States, with an estimated 37.5–38 million visitors annually, and one of the most filmed locations in the world.", price: "700 000", photo: "https://images.unsplash.com/photo-1542622805-980533ee81ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80")
+
+Monument.create(title: 'Yellowstone National Park', address: 'Yellowstone National Park, WY 82190, USA', description: "An American national park located mostly in Wyoming, with small sections in Montana and Idaho.", price: "300 000", photo: "https://images.unsplash.com/photo-1550347778-473e0058577c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80")
+
+Monument.create(title: 'Buckingham Palace', address: 'Westminster, London SW1A 1AA, UK', description: "The London residence and administrative headquarters of the monarch of the United Kingdom.", price: "500 000", photo: "https://images.unsplash.com/photo-1566594918224-1bc366f40de9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80")
+
+Monument.create(title: 'Palace of Versailles', address: "Place d'Armes, 78000 Versailles, France", description: "The principal royal residence of France from 1682, under Louis XIV, until the start of the French Revolution in 1789, under Louis XVI. In 2017 the Palace of Versailles received 7,700,000 visitors, making it the second-most visited monument in Paris region.", price: "800 000", photo: "https://images.unsplash.com/photo-1566087247505-cd95ebd53df9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80")
+
+Monument.create(title: 'Taj Mahal', address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India", description: "An ivory-white marble Islamic mausoleum on the south bank of the Yamuna river in the Indian city of Agra.", price: "700 000", photo: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80")
+
+Monument.create(title: 'Cristo Redentor', address: "Corcovado Mountain, Tijuca Forest National Park, Rio de Janeiro, Brazil", description: "An Art Deco statue of Jesus Christ in Rio de Janeiro, created by French sculptor Paul Landowski. The statue weighs 635 metric tons (625 long, 700 short tons), and is located at the peak of the 700-metre (2,300 ft) Corcovado mountain, the statue has become a cultural icon of both Rio de Janeiro and Brazil, and is listed as one of the New7Wonders of the World.", price: "600 000", photo: "https://images.unsplash.com/photo-1548533701-a1e8bd19f9ce?ixlib=rb-1.2.1&auto=format&fit=crop&w=345&q=80")
+
+Monument.create(title: 'Villa Gaudelet', address: "16 Villa Gaudelet, 75011 Paris, France", description: "Le Wagon is ranked as the world's best coding bootcamp. The Villa Gaudelet is the headquarter of ", price: "600 000", photo: "http://bindies.fr/wp-content/uploads/2016/11/LEWAGON_20151210_HD-1.jpg")
+
+
+
+
+
