965b21937d74141997ac0cf807a4dfa632461f96
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-11-18 17:26:43 +0100

    monument controller and views - cloudinary

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -5,4 +5,7 @@ class Monument < ApplicationRecord
   validates :address, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: true
+  validates :photo, presence: true
+  has_many_attached :photos
+  has_one_attached :photo
 end
