4d5054a316173abb13bea42791f733779840c3d9
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-20 13:27:06 +0100

    carousel and show updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,23 @@
 import "bootstrap";
+import Siema from 'siema';
+
+
+
+const mySiema = new Siema({
+  selector: '.siema',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: false,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+  });
+
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
