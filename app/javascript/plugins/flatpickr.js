e0b6c35d6a86cf6a04a1451c75c9c401a47daad7
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-11-21 17:25:31 +0100

    js refacto

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,7 +2,11 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr("#start_date", {
-  altInput: true,
-  plugins: [new rangePlugin({ input: "#end_date"})]
-})
+const initFlatpickr = () => {
+  flatpickr("#start_date", {
+    altInput: true,
+    plugins: [new rangePlugin({ input: "#end_date"})]
+  })
+}
+
+export { initFlatpickr };
