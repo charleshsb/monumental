086d6da723d1d9d558f2cf2a08360136e5558b44
Martin Marinovic <mgnavarm@yahoo.com>
2019-11-22 13:36:39 +0100

    tabs ok, some format missing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import "bootstrap";
 import { initSiema } from "../plugins/siema";
 import { initFlatpickr } from "../plugins/flatpickr";
-
+import { toggleTabs } from "../plugins/tabs";
 
 initFlatpickr();
 initSiema();
+toggleTabs();
+
