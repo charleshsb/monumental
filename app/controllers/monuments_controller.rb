1b2d82e75360bd1bd0128530d0873e5ecb8ab77a
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-11-22 15:48:11 +0100

    seed + modal

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -20,12 +20,12 @@ class MonumentsController < ApplicationController
     else
       @monuments = Monument.all
     end
-
   end
 
   def show
     @monument = Monument.find(params[:id])
     @booking = Booking.new
+    @user = current_user
   end
 
   def edit
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
 import { mySiema } from "../plugins/siema";
 import { initFlatpickr } from "../plugins/flatpickr";
+import { priceForModal, confirmBooking } from "../plugins/modal";
 
 initFlatpickr();
-mySiema();
+// mySiema();
+priceForModal();
+confirmBooking();
